d8b91b738f51c7f374acc7eb2b9c9772c65e6acc
Rakesh Gupta <50867187+Rakesh4G@users.noreply.github.com>
2019-08-05 11:24:59 +0530

    Create default.nix for tiledb library

---
+++ pkgs/development/libraries/tiledb/default.nix
@@ -0,0 +1,57 @@
+{
+lib
+,stdenv
+,fetchgit
+,cmake
+,zlib
+,lz4
+,bzip2
+,zstd
+,spdlog_0
+,tbb
+,openssl
+,boost
+,libpqxx
+,clang-tools
+,catch2
+,python
+,gtest
+,doxygen
+}:
+
+stdenv.mkDerivation {
+    name = "tiledb";
+    version = "1.5.1";
+
+  src = fetchgit {
+    url = "https://github.com/TileDB-Inc/TileDB";
+    rev = "7236bb29f2aa1ac4de489f0791e9265cb257184d";
+    sha256 = "sha256:0ky0dcv1w1jn1cjn3819aq9xyd2wg80aagf2flxmd916flgr9zjl";
+  };
+
+  makeTarget = "tiledb";
+
+  outputs = ["out"];
+
+  nativeBuildInputs = [clang-tools doxygen  gtest];
+
+  buildInputs = [cmake catch2 zlib lz4 bzip2 zstd spdlog_0 tbb openssl boost libpqxx python ];
+  
+  postPatch = ''
+    rm -rf external/Catch2
+    ln -sf ${catch2.src} external/Catch2
+  '';
+
+  cmakeFlags = [
+  "-DCATCH_INCLUDE_DIR=${catch2}"
+  "-DTILEDB_SUPERBUILD=OFF"
+#  "-DINCLUDE_DIR=${clang-tools}/bin"
+  ];
+
+  meta = with lib; {
+    description = "TileDB allows you to manage the massive dense and sparse multi-dimensional array data";
+    homepage = https://github.com/TileDB-Inc/TileDB;
+    license = licenses.mit;
+    maintainers = with maintainers; [ rakesh4g ];
+  };
+}
