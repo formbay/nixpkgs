21bce80340f0545540d3dbcfa505a5d0434cbdef
Rakesh Gupta <50867187+Rakesh4G@users.noreply.github.com>
2019-08-26 22:03:48 +1000

    imgaug: 0.2.9-patch
    Added patch to bypass opencv-python dependency from imgaug
    
    @CMCDragonkai

--- pkgs/development/python-modules/imgaug/default.nix
+++ pkgs/development/python-modules/imgaug/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildPythonPackage, fetchPypi, numpy, scipy, scikitimage, opencv3, six }:
+{ stdenv, buildPythonPackage, fetchPypi, numpy, scipy, scikitimage, opencv3, six, imageio, shapely }:
 
 buildPythonPackage rec {
   pname = "imgaug";
@@ -9,12 +9,21 @@ buildPythonPackage rec {
     sha256 = "42b0c4c8cbe197d4f5dbd33960a1140f8a0d9c22c0a8851306ecbbc032092de8";
   };
 
+  installPhase = ''
+    substituteInPlace requirements.txt \
+      --replace "opencv-python" "opencv>=3.4.7"
+    substituteInPlace setup.py \
+      --replace "opencv-python" "opencv>=3.4.7"
+  '';
+
   propagatedBuildInputs = [
     numpy
     scipy
     scikitimage
     opencv3
     six
+    imageio
+    shapely
   ];
 
   # disable tests when there are no tests in the PyPI archive
@@ -25,6 +34,5 @@ buildPythonPackage rec {
     description = "Image augmentation for machine learning experiments";
     license = licenses.mit;
     maintainers = with maintainers; [ cmcdragonkai ];
-    broken = true; # opencv-python bindings aren't available yet, and look non-trivial
   };
 }
