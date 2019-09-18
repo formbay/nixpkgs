06b1f020b0f8a916549d3bc75ab2cc60f7981bff
Rakesh Gupta <rakeshgupta4u@gmail.com>
2019-09-18 15:34:59 +0530

    fixed error on latest version

--- pkgs/development/python-modules/tiledbpy/default.nix
+++ pkgs/development/python-modules/tiledbpy/default.nix
@@ -7,29 +7,31 @@
 , tiledb
 , numpy
 , wheel
+#, cmake
 }:
 
 buildPythonPackage rec {
   pname = "tiledb";
   version = "0.4.2";
 
-#  src = fetchPypi {
-#    inherit pname version;
-#    sha256 = "bda0ef48e6a44c091399b12ab4a7e580d2dd8294c222b301f88d7d57f47ba142";
-#  };
-  
-  src = fetchFromGitHub {
-    owner = "TileDB-Inc";
-    repo = "TileDB-Py";    
-    rev = "0.4.3";   
-    sha256 = "sha256:16hdz2d5d4llrq3l08a846yx4s71i780irhak041vjn1j1n3wmz6";
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "bda0ef48e6a44c091399b12ab4a7e580d2dd8294c222b301f88d7d57f47ba142";
   };
+  
+#  src = fetchFromGitHub {
+#    owner = "TileDB-Inc";
+#    repo = "TileDB-Py";    
+#    rev = "0.4.3";   
+#    sha256 = "sha256:16hdz2d5d4llrq3l08a846yx4s71i780irhak041vjn1j1n3wmz6";
+#  };
 
   nativeBuildInputs = [
     setuptools_scm
     numpy
     wheel
     tiledb
+#    cmake
   ];
 
   propagatedBuildInputs = [
